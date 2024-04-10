2ad91076c28484a420911d9d249807165ae9a86c
Hayeon LEE <fhdodl@naver.com>
2024-04-10 23:39:43 +0900

    feat: shadow 스타일 적용

---
+++ src/foundation/styles/shadow.ts
@@ -0,0 +1,8 @@
+export const shadow = {
+  small:
+    'rgba(50, 50, 105, 0.25) 0px 4px 8px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px',
+  medium:
+    'rgba(50, 50, 105, 0.4) 0px 6px 12px 0px, rgba(0, 0, 0, 0.15) 0px 3px 6px 0px',
+  large:
+    'rgba(50, 50, 105, 0.5) 0px 8px 16px 0px, rgba(0, 0, 0, 0.2) 0px 4px 8px 0px',
+} as const;
