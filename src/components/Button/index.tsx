1e4fcf0e356dbcbe30bd483067ae83de0841e52b
Hayeon LEE <fhdodl@naver.com>
2024-04-13 23:47:08 +0900

    refactor: 공통으로 사용되는 타입과 스타일 관련 코드 분리

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes, forwardRef } from 'react';
+import React, { ButtonHTMLAttributes, forwardRef, ForwardedRef } from 'react';
 import styled from '@emotion/styled';
 import { getPaddingStyle, SpacingStyles } from '../../foundation/spacing';
 import {
@@ -17,7 +17,10 @@ import {
   gray300,
   white,
 } from '../../foundation/color';
-
+import {
+  ComponentBorderKey,
+  BORDER_RADIUS_SET,
+} from '../../foundation/styles/border';
 type ButtonState = {
   default: string;
   hover: string;
@@ -26,15 +29,14 @@ type ButtonState = {
 };
 
 type ButtonColorSet = {
-  [key: string]: {
-    background: ButtonState;
-    border: ButtonState;
-    text: ButtonState;
-  };
+  background: ButtonState;
+  border: ButtonState;
+  text: ButtonState;
 };
+type ButtonVariant = keyof typeof COLOR_SET;
 
 // variant에 따라 버튼의 색상이 달라져야 하기 때문에 지정
-const colorSets: ButtonColorSet = {
+const COLOR_SET: Record<string, ButtonColorSet> = {
   primary: {
     background: {
       default: orange500,
@@ -77,133 +79,133 @@ const colorSets: ButtonColorSet = {
   },
 };
 
+export type ComponentSizeKey = 'xs' | 'sm' | 'md' | 'lg';
+export type ComponentSizeSet = Record<ComponentSizeKey, SpacingStyles>;
 /**
- * 버튼 사이즈 : 패딩 값에 의해 버튼 사이즈가 xs,sm,md,lg로 나뉜다.
+ *  사이즈 : 패딩 값에 의해 버튼 사이즈가 xs,sm,md,lg로 나뉜다.
  */
-const PADDING_SET: { [key: string]: SpacingStyles } = {
+const PADDING_SET: ComponentSizeSet = {
   xs: getPaddingStyle(2, 4),
   sm: getPaddingStyle(4, 4),
   md: getPaddingStyle(4, 12),
   lg: getPaddingStyle(6, 18),
 } as const;
-
 /**
- * 버튼 폰트 사이즈 : 버튼 사이즈에 따라 다르게 설정
+ * 폰트 사이즈 : 버튼 사이즈에 따라 다르게 설정
  */
-const FONT_SIZE_SET: { [key: string]: string } = {
+const FONT_SIZE_SET: Record<keyof ComponentSizeSet, string> = {
   xs: '0.75rem',
   sm: '0.875rem',
   md: '1rem',
   lg: '2rem',
 } as const;
 
-/**
- * 버튼 border 스타일 : square,rounded,default
- */
-const BORDER_RADIUS_SET: { [key: string]: string } = {
-  square: '0',
-  rounded: '9999999px',
-  default: '5px',
-} as const;
-
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: keyof ButtonColorSet;
+  variant?: ButtonVariant;
   fullWidth?: boolean;
-  radius?: 'square' | 'rounded' | 'default';
+  radius?: ComponentBorderKey;
   leftIcon?: React.ReactElement;
   rightIcon?: React.ReactElement;
   icon?: React.ReactElement;
   textButton?: boolean;
   outline?: boolean;
-  size?: keyof typeof PADDING_SET;
+  size?: ComponentSizeKey;
 }
 
-export const Button: React.FC<ButtonProps> = forwardRef<
-  HTMLButtonElement,
-  ButtonProps
->(
-  (
-    {
-      children,
-      variant = 'primary',
-      fullWidth = false,
-      radius = 'default',
-      leftIcon,
-      rightIcon,
-      icon,
-      textButton,
-      outline = false,
-      size = 'md',
-      ...props
-    },
-    ref,
-  ) => {
-    const colorSet = colorSets[variant];
-    const BaseButton = styled.button`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border: ${outline ? `1px solid ${colorSet.border.default}` : 'none'};
-      background-color: ${colorSet.background.default};
-      color: ${colorSet.text.default};
-      ${PADDING_SET[size]};
-      border-radius: ${BORDER_RADIUS_SET[radius]};
-      font-size: ${FONT_SIZE_SET[size]};
-      width: ${fullWidth ? '100%' : 'auto'};
-      cursor: pointer;
-      transition: all 0.3s ease;
+export const Button = (
+  {
+    children,
+    variant = 'primary',
+    fullWidth = false,
+    radius = 'default',
+    leftIcon,
+    rightIcon,
+    icon,
+    textButton,
+    outline = false,
+    size = 'md',
+    ...props
+  }: ButtonProps,
+  ref: ForwardedRef<HTMLButtonElement>,
+) => {
+  const colorSet = COLOR_SET[variant];
 
-      /*  아이콘 스타일 */
-      div {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        &.leftIcon {
-          margin-right: 4px;
-        }
-        &.rightIcon {
-          margin-left: 4px;
-        }
-      }
+  return (
+    <BaseButton
+      ref={ref}
+      colorSet={colorSet}
+      outline={outline}
+      fullWidth={fullWidth}
+      radius={radius}
+      size={size}
+      {...props}
+    >
+      {leftIcon && (
+        <IconContainer className="leftIcon">{leftIcon}</IconContainer>
+      )}
+      {children}
+      {rightIcon && (
+        <IconContainer className="rightIcon">{rightIcon}</IconContainer>
+      )}
+      {icon && <IconContainer>{icon}</IconContainer>}
+    </BaseButton>
+  );
+};
 
-      &:hover {
-        background-color: ${colorSet.background.hover};
-        color: ${colorSet.text.hover};
-      }
+const BaseButton = styled.button<{
+  colorSet: ButtonColorSet;
+  outline: boolean;
+  fullWidth: boolean;
+  radius: ComponentBorderKey;
+  size: ComponentSizeKey;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: ${({ outline, colorSet }) =>
+    outline ? `1px solid ${colorSet.border.default}` : 'none'};
+  background-color: ${({ colorSet }) => colorSet.background.default};
+  color: ${({ colorSet }) => colorSet.text.default};
+  ${({ size }) => PADDING_SET[size]};
+  border-radius: ${({ radius }) => BORDER_RADIUS_SET[radius]};
+  font-size: ${({ size }) => FONT_SIZE_SET[size]};
+  width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
+  cursor: pointer;
+  transition: all 0.3s ease;
 
-      &:active {
-        background-color: ${colorSet.background.active};
-        color: ${colorSet.text.active};
-      }
+  &:hover {
+    background-color: ${({ colorSet }) => colorSet.background.hover};
+    color: ${({ colorSet }) => colorSet.text.hover};
+  }
 
-      &:disabled {
-        background-color: ${colorSet.background.disabled};
-        border-color: ${colorSet.border.disabled};
-        color: ${colorSet.text.disabled};
-        cursor: not-allowed;
-      }
+  &:active {
+    background-color: ${({ colorSet }) => colorSet.background.active};
+    color: ${({ colorSet }) => colorSet.text.active};
+  }
 
-      /* 키보드로 버튼에 포커스 시 */
-      &:focus-visible {
-        outline: 1px solid ${colorSet.border.active};
-      }
-      /* 마우스, 터치로 버튼에 포커스 시
-      &:focus:not(:focus-visible) {
-        outline: none;
-        border: 1px solid ${colorSet.border.hover};
-      } */
-    `;
+  &:disabled {
+    background-color: ${({ colorSet }) => colorSet.background.disabled};
+    border-color: ${({ colorSet }) => colorSet.border.disabled};
+    color: ${({ colorSet }) => colorSet.text.disabled};
+    cursor: not-allowed;
+  }
 
-    return (
-      <BaseButton ref={ref} {...props}>
-        {leftIcon && <div className="leftIcon">{leftIcon}</div>}
-        {children}
-        {rightIcon && <div className="rightIcon">{rightIcon}</div>}
-        {icon && <div>{icon}</div>}
-      </BaseButton>
-    );
-  },
-);
-Button.displayName = 'Button';
+  /* 키보드로 버튼에 포커스 시 */
+  &:focus-visible {
+    outline: 1px solid ${({ colorSet }) => colorSet.border.active};
+  }
+`;
 
-export default Button;
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &.leftIcon {
+    margin-right: 4px;
+  }
+  &.rightIcon {
+    margin-left: 4px;
+  }
+`;
+
+export default forwardRef(Button);
