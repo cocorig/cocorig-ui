63044768e90ea19000596971cdae89e62a464ed2
Hayeon LEE <fhdodl@naver.com>
2024-04-14 23:37:36 +0900

    refactor: 공통 size 스타일 적용

--- src/foundation/typography/common/typographyStyle.ts
+++ src/foundation/typography/common/typographyStyle.ts
@@ -1,67 +1,49 @@
 import { css } from '@emotion/react';
-import { spacing, SpacingProps } from '../../spacing';
+import {
+  SpacingPropType,
+  SerializedType,
+  getSpacingStyles,
+} from '../../spacing';
+import { SIZE_SET, SizeKeyType } from '../../styles/size';
 
-export type TypographyProps = SpacingProps & {
+export interface TypographyProps extends SpacingPropType {
   color?: string;
-  size?: 'xxxl' | 'xxl' | 'xl' | 'l' | 'm' | 's' | 'xs' | 'xxs' | number;
+  size?: SizeKeyType | number;
   textAlign?: 'center' | 'left' | 'right' | 'inherit' | 'justify';
-  weight?: 'extraBold' | 'bold' | 'regular';
+  weight?: WeightProps;
   letterSpacing?: string | number;
   lineHeight?: string | number;
   textDecoration?: string | 'line-through' | 'underline';
+}
+
+const WEIGHT_SET = {
+  extraBold: 700,
+  bold: 500,
+  regular: 400,
+} as const;
+
+export type WeightProps = keyof typeof WEIGHT_SET;
+
+export const getSizeStyles = (size: SizeKeyType | number): string => {
+  return typeof size === 'number' ? `${size}px` : SIZE_SET[size];
 };
 
-export const SizeStyles = (size: TypographyProps['size']): string => {
-  const sizeMap: { [key: string]: string } = {
-    xxxl: '1.5rem',
-    xxl: '1.375rem',
-    xl: '1.25rem',
-    l: '1.125rem',
-    m: '1rem',
-    s: '0.875rem',
-    xs: '0.75rem',
-    xxs: '0.6875rem',
-  } as const;
-
-  if (typeof size === 'number') {
-    return `${size}px`;
-  } else {
-    return sizeMap[size as string] || sizeMap.m;
-  }
-};
-
-export const WeightStyles = (weight: TypographyProps['weight']): number => {
-  const weightMap: { [key: string]: number } = {
-    extraBold: 700,
-    bold: 500,
-    regular: 400,
-  } as const;
-  return weightMap[weight as string] || weightMap.regular;
-};
-
-export const SpacingStyles = (
-  spacingProps: Partial<SpacingProps>,
-): ReturnType<typeof css> => {
-  const styles = Object.entries(spacingProps)
-    .filter(([prop, value]) => typeof value === 'number' && spacing[prop])
-    .map(([prop, value]) => spacing[prop](value as number));
-  return css`
-    ${styles}
-  `;
+export const getWeightStyles = (weight: WeightProps): number => {
+  return WEIGHT_SET[weight];
 };
 
 export const getTypographyStyles = ({
   color,
-  size,
+  size = 'md',
   textAlign = 'left',
-  weight,
+  weight = 'regular',
   letterSpacing,
   lineHeight,
   textDecoration,
   ...spacingProps
-}: TypographyProps): ReturnType<typeof css> => {
-  const fontSize = SizeStyles(size);
-  const fontWeight = WeightStyles(weight);
+}: TypographyProps): SerializedType => {
+  const fontSize = getSizeStyles(size);
+  const fontWeight = getWeightStyles(weight);
   return css`
     color: ${color};
     font-size: ${fontSize};
@@ -70,6 +52,6 @@ export const getTypographyStyles = ({
     letter-spacing: ${letterSpacing};
     line-height: ${lineHeight};
     text-decoration: ${textDecoration};
-    ${SpacingStyles(spacingProps)};
+    ${getSpacingStyles(spacingProps)};
   `;
 };
