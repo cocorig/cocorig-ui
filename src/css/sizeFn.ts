18484b8005ffdf410f1c1199d4649f87b63fc653
Hayeon LEE <fhdodl@naver.com>
2024-12-29 23:52:45 +0900

    feat: BOX_SIZE_SET에 정의된 값에 따라 폰트 크기, 라인 높이, 패딩, 높이를 설정하는 스타일 함수

---
+++ src/css/sizeFn.ts
@@ -0,0 +1,28 @@
+import { css } from '@emotion/react';
+
+import { BOX_SIZE_SET, SizesToken } from '../foundation';
+
+import { toSizeUnit } from './toSizeUnit';
+
+
+export const fontStyle = (size: SizesToken) => {
+  const { fontSize, lineHeight } = BOX_SIZE_SET[size];
+  return css`
+    font-size: ${toSizeUnit(fontSize)};
+    line-height: ${toSizeUnit(lineHeight)};
+  `;
+};
+export const BoxSizeStyle = (size: SizesToken) => {
+  const { padding, height } = BOX_SIZE_SET[size];
+  return css`
+    padding-inline-start: ${toSizeUnit(padding)};
+    padding-inline-end: ${toSizeUnit(padding)};
+    height: ${toSizeUnit(height)};
+  `;
+};
+export const getSizesAndFontSize = (size: SizesToken) => {
+  return css`
+    ${fontStyle(size)}
+    ${BoxSizeStyle(size)}
+  `;
+};
