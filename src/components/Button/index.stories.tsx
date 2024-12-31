daefe06f8129295ded0ce6a5c267b5a2d6cdfe64
Hayeon LEE <fhdodl@naver.com>
2024-12-31 22:14:03 +0900

    feat: 버튼 스토리 추가

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,73 +1,87 @@
 import React from 'react';
-import { Meta, StoryObj } from '@storybook/react';
-import Button from '.';
 
-const meta: Meta<typeof Button> = {
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Body, colorPaletteOptions } from '../../foundation';
+import { getDefaultValue } from '../../storybook-props';
+import { commonArgTypes } from '../Badge/index.stories';
+import { HStack, VStack } from '../Stack';
+
+import { Button } from '.';
+const meta = {
   title: 'Components/Button',
   component: Button,
+  parameters: {},
+  args: {
+    children: 'Button',
+    size: 'md',
+    colorScheme: 'gray',
+    variant: 'base',
+    borderRadius: 'default',
+    weight: 'bold',
+  },
   argTypes: {
-    variant: {
-      control: {
-        type: 'select',
-        options: ['primary', 'secondary'],
-      },
-    },
-    children: {
-      control: 'text',
-    },
-    disabled: {
-      control: 'boolean',
-    },
-    fullWidth: {
-      control: 'boolean',
-    },
-    radius: {
-      control: {
-        type: 'select',
-        options: ['square', 'rounded', 'default'],
-      },
-    },
-    size: {
-      control: {
-        type: 'select',
-        options: ['xs', 'sm', 'md', 'lg'],
+    ...commonArgTypes,
+    iconSpacing: {
+      table: {
+        ...getDefaultValue('12px'),
       },
     },
   },
-};
+} satisfies Meta<typeof Button>;
+
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = (args) => <Button {...args} />;
-Primary.args = {
-  variant: 'primary',
-  children: 'Primary Button',
+export const Default: Story = {};
+
+const variantOptions = ['base', 'soft', 'subtle', 'outline', 'ghost'] as const;
+export const ButtonVariants: Story = {
+  render: () => (
+    <VStack gap={0.5} alignItems="flex-start">
+      {variantOptions.map((variant) => (
+        <HStack gap={2} alignItems="center" key={variant}>
+          <Body size="md" style={{ minWidth: '10ch' }}>
+            {variant}
+          </Body>
+          <Button variant={variant}>Button</Button>
+        </HStack>
+      ))}
+    </VStack>
+  ),
 };
 
-export const Secondary: Story = (args) => <Button {...args} />;
-Secondary.args = {
-  variant: 'secondary',
-  children: 'Secondary Button',
+export const ButtonColors: Story = {
+  render: () => (
+    <VStack gap={0.5} alignItems="flex-start">
+      {colorPaletteOptions.map((color) => (
+        <HStack gap={2} alignItems="center" key={color}>
+          <Body size="md" style={{ minWidth: '10ch' }}>
+            {color}
+          </Body>
+          <Button variant="base" colorScheme={color}>
+            Button
+          </Button>
+        </HStack>
+      ))}
+    </VStack>
+  ),
 };
 
-export const WithIcons: Story = (args) => <Button {...args} />;
-WithIcons.args = {
-  variant: 'primary',
-  children: 'Button with Icons',
-  leftIcon: <span>🤗</span>,
-  rightIcon: <span>🔥</span>,
-};
-
-export const FullWidth: Story = (args) => <Button {...args} />;
-FullWidth.args = {
-  variant: 'primary',
-  children: 'Full Width Button',
-  fullWidth: true,
-};
-
-export const Disabled: Story = (args) => <Button {...args} />;
-Disabled.args = {
-  variant: 'primary',
-  children: 'Disabled Button',
-  disabled: true,
+export const AllButtonVariantsAndColors: Story = {
+  render: () => (
+    <VStack gap={0.5} alignItems="flex-start">
+      {colorPaletteOptions.map((color) => (
+        <HStack gap={2} alignItems="center" key={color}>
+          {variantOptions.map((variant) => (
+            <HStack gap={2} alignItems="center">
+              <Button key={`${color}-${variant}`} variant={variant} colorScheme={color}>
+                Button
+              </Button>
+            </HStack>
+          ))}
+        </HStack>
+      ))}
+    </VStack>
+  ),
 };
