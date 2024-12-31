0145ca4b9d53ba37d86c1ec7a194660ac25296ae
Hayeon LEE <fhdodl@naver.com>
2024-12-31 22:04:04 +0900

    feat: 아코디언 스토리 추가

---
+++ src/components/Accordion/index.stories.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+import { colorPaletteOptions, Heading } from '../../foundation';
+import { getPropsType } from '../../storybook-props';
+import { VStack } from '../Stack';
+
+import { Accordion } from '.';
+
+const items = [
+  { title: '1일차', text: '공항에 도착 후 호텔 체크인' },
+  { title: '2일차', text: '지역 시장 탐방 후 현지 음식 시도' },
+  { title: '3일차', text: '유명한 관광지 방문 후 시티 투어' },
+  { title: '4일차', text: '해변에서 휴식 후 석양 감상' },
+];
+
+const renderAccordionItems = (items) => {
+  return items.map((item, index) => (
+    <Accordion.Item key={index} itemValue={index}>
+      <Accordion.Header icon={<Accordion.Icon />}>{item.title}</Accordion.Header>
+      <Accordion.Body>{item.text}</Accordion.Body>
+    </Accordion.Item>
+  ));
+};
+
+const meta = {
+  title: 'Components/Accordion',
+  component: Accordion,
+  parameters: {},
+
+  render: (args) => <Accordion {...args}>{renderAccordionItems(items)}</Accordion>,
+  args: {
+    size: 'md',
+    variant: 'underlined',
+    colorScheme: 'gray',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'radio',
+      },
+    },
+    variant: {
+      options: ['filled', 'underlined'],
+      control: {
+        type: 'radio',
+      },
+    },
+    colorScheme: {
+      options: colorPaletteOptions,
+      control: {
+        type: 'select',
+      },
+    },
+
+    allowMultiple: {
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        ...getPropsType('boolean'),
+      },
+      description: '여러 항목을 동시에 열 수 있는지 여부를 설정합니다.',
+    },
+    defaultId: {
+      table: {
+        ...getPropsType('number[]'),
+      },
+      description: '기본적으로 열릴 아이템의 고유 ID 배열을 설정합니다.',
+    },
+  },
+} satisfies Meta<typeof Accordion>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+const sizes = ['sm', 'md', 'lg'] as const;
+
+export const Sizes: Story = {
+  render: () => (
+    <VStack gap={1}>
+      {sizes.map((size) => (
+        <React.Fragment key={size}>
+          <Heading weight="bold">{size}</Heading>
+          <Accordion size={size}>{renderAccordionItems(items)}</Accordion>
+        </React.Fragment>
+      ))}
+    </VStack>
+  ),
+};
+export const FilledVariant: Story = {
+  args: {
+    variant: 'filled',
+  },
+};
+
+export const AllowMultipleExpansion: Story = {
+  args: {
+    allowMultiple: true,
+  },
+};
+
+export const InitialAccordionState: Story = {
+  args: {
+    defaultId: [1],
+  },
+};
