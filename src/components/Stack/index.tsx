d741e2bada5f739513bba885424380f8f3ad9361
Hayeon LEE <fhdodl@naver.com>
2024-12-31 23:02:03 +0900

    feat: Stack 컴포넌트 추가

---
+++ src/components/Stack/index.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+import { vStack, hStack, stack, StackProps, FixedDirectionStackProps } from '../../css';
+
+export const VStack = styled.div<FixedDirectionStackProps>`
+  ${vStack}
+`;
+
+export const HStack = styled.div<FixedDirectionStackProps>`
+  ${hStack}
+`;
+
+export const Stack = styled.div<StackProps>`
+  ${stack}
+`;
