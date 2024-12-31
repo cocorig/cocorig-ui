bd67f9128f3d797ca491f85a9faf8e723506745b
Hayeon LEE <fhdodl@naver.com>
2025-01-01 00:24:23 +0900

    feat: rollup 번들링 및 타입 설정 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,98 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import terser from "@rollup/plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import dts from "rollup-plugin-dts";
-import svgr from "@svgr/rollup";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-const packageJson = require("./package.json");
+import fs from 'fs';
+import path from 'path';
 
+const extensions = ['.js', 'jsx', '.ts', '.tsx'];
+
+// 공통 플러그인 설정
+const commonPlugins = [
+  resolve({ extensions }),
+  commonjs(),
+  terser(),
+  peerDepsExternal(),
+  typescript({ tsconfig: './tsconfig.json' }),
+];
+
+// 공통 Output 설정
+const commonOutputOptions = {
+  preserveModules: true,
+  preserveModulesRoot: 'src',
+};
+
+// external 옵션
+const externalDependencies = [/node_modules/, /react/, /@emotion/];
+
+/**
+ * 디렉토리 내에서 index.ts와 index.tsx 파일만 찾는 함수
+ * @param {string} dir - 탐색할 디렉토리
+ * @returns {string[]} - index.ts와 index.tsx 파일 경로 리스트
+ */
+const findIndexFiles = (dir) => {
+  const results = [];
+  const stack = [dir];
+
+  while (stack.length > 0) {
+    const currentDir = stack.pop();
+    const files = fs.readdirSync(currentDir);
+
+    for (const file of files) {
+      const filePath = path.join(currentDir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        stack.push(filePath);
+      } else if (/^index\.(ts|tsx)$/.test(file)) {
+        results.push(filePath);
+      }
+    }
+  }
+  return results;
+};
+
+const srcDir = 'src';
+const indexFiles = findIndexFiles(srcDir);
+console.log(indexFiles);
 export default [
   {
-    input: "src/index.ts",
+    input: [...indexFiles],
     output: [
       {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
+        dir: 'dist/cjs',
+        format: 'cjs',
+        ...commonOutputOptions,
       },
+
       {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
+        dir: 'dist/esm',
+        format: 'esm',
+        ...commonOutputOptions,
       },
     ],
-    plugins: [
-      typescript(),
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
-      postcss({
-        modules: true,
-        use: ["sass"],
-      }),
-      svgr(),
-    ],
+    external: externalDependencies,
+    plugins: [...commonPlugins],
   },
+
+  // types
   {
-    input: "dist/esm/types/src/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts.default()],
-    external: [/\.css$/],
+    input: ['src/index.ts'],
+    output: [
+      {
+        dir: 'dist/types',
+        format: 'esm',
+      },
+    ],
+    external: externalDependencies,
+    plugins: [
+      ...commonPlugins,
+      typescript({
+        declarationDir: 'dist/types',
+        declaration: true,
+      }),
+    ],
   },
 ];
