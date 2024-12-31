2ddee750b606721cae80ace3a89f02c779d1d6fc
Hayeon LEE <fhdodl@naver.com>
2024-12-31 21:00:04 +0900

    feat: storybook 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,55 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['Configure.mdx', '../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)'],
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      // union prop type을 select 컨트롤로 생성
+      shouldExtractLiteralValuesFromEnum: true,
+      // string,boolean type을 inputs 혹은 switches 컨트롤로 생성
+      shouldRemoveUndefinedFromOptional: true,
+    },
+  },
   addons: [
-    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-docs',
     '@storybook/addon-onboarding',
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
+
+  webpackFinal: async (config) => {
+    config.module?.rules?.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic',
+                  importSource: '@emotion/react',
+                },
+              ],
+              '@babel/preset-typescript',
+            ],
+            plugins: ['@emotion/babel-plugin'],
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
 export default config;
---
+++ .storybook/preview.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Global } from '@emotion/react';
+import type { Preview } from '@storybook/react';
+import { baseStyle } from '../src/css/baseStyle';
+
+const GlobalStyle = () => <Global styles={baseStyle} />;
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+      expanded: true,
+    },
+    a11y: {
+      element: '#root',
+      manual: false,
+    },
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+
+  decorators: [
+    (Story) => (
+      <>
+        <GlobalStyle />
+        <Story />
+      </>
+    ),
+  ],
+};
+
+export default preview;
