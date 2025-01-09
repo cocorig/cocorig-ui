7e861881f4862704f6b2e4d5021e30407ff5a3f3
Hayeon LEE <fhdodl@naver.com>
2025-01-10 00:03:35 +0900

    fix: 텍스트 없는 아이콘 버튼에 aria-label 속성 추가하여 접근성 개선

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -5,13 +5,15 @@ import styled from '@emotion/styled';
 import { Button } from '../Button';
 import { ButtonProps } from '../Button/type';
 
-export const IconButton = forwardRef(({ children, ...props }: ButtonProps, ref: Ref<HTMLButtonElement>) => {
-  return (
-    <IButton {...props} ref={ref}>
-      {children}
-    </IButton>
-  );
-});
+export const IconButton = forwardRef(
+  ({ children, label, ...props }: ButtonProps & { label: string }, ref: Ref<HTMLButtonElement>) => {
+    return (
+      <IButton {...props} ref={ref} aria-label={label}>
+        {children}
+      </IButton>
+    );
+  },
+);
 
 export const IButton = styled(Button)`
   padding: unset;
