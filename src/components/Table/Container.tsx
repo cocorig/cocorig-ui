407445308d99008317507b4868e5416c8cf4aa2e
Hayeon LEE <fhdodl@naver.com>
2025-01-24 13:54:27 +0900

    fix: Table.Container 내의 table 요소에서 border 스타일 제거

--- src/components/Table/Container.tsx
+++ src/components/Table/Container.tsx
@@ -7,8 +7,11 @@ import { TableVariantProps } from '../../styled-system';
 import { toSizeUnit } from '../../utils';
 
 export interface Container extends HTMLAttributes<HTMLDivElement>, Pick<TableVariantProps, 'colorScheme'> {
+  /** 테이블의 `높이`를 설정합니다. number나 string 형태로 지정할 수 있습니다. */
   height: number | string;
+  /** 테이블의 `테두리 반지름`을 설정합니다. 기본값은 `default`입니다. */
   borderRadius?: BordersToken;
+  /** 테이블의 `테두리 두께`를 설정합니다. */
   borderWidth?: string | number;
 }
 export const Container = ({ children, ...props }: Container) => {
@@ -34,6 +37,8 @@ const StyledContainer = styled.div<Container>`
 
   & table {
     overflow: auto;
+    box-shadow: none;
+    border-radius: 0;
   }
   & thead {
     position: sticky;
