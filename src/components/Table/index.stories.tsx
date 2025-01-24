85198be2d362b4e9be010afd78355cb25d61afca
Hayeon LEE <fhdodl@naver.com>
2025-01-24 13:55:28 +0900

    docs: 서브 컴포넌트가 스토리에 적용되도록 변경

--- src/components/Table/index.stories.tsx
+++ src/components/Table/index.stories.tsx
@@ -10,6 +10,14 @@ import { Table } from '.';
 const meta = {
   title: 'Components/Table',
   component: Table,
+  subcomponents: {
+    'Table.Container': Table.Container,
+    'Table.Head': Table.Head,
+    'Table.ColumnHeader': Table.ColumnHeader,
+    'Table.Body': Table.Body,
+    'Table.Cell': Table.Cell,
+    'Table.Row': Table.Row,
+  },
   args: {
     variant: 'outline',
     size: 'sm',
@@ -60,7 +68,7 @@ const TableHeader = () => (
   </Table.Head>
 );
 
-const TableBody = ({ data }: { data: Array<{ id: number; name: string; department: string; salary: number }> }) => (
+const TableBody = (data: Array<{ id: number; name: string; department: string; salary: number }>) => (
   <Table.Body>
     {data.map((item) => (
       <Table.Row key={item.id}>
@@ -72,22 +80,82 @@ const TableBody = ({ data }: { data: Array<{ id: number; name: string; departmen
   </Table.Body>
 );
 
+const defaultCode = `
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            <Table.ColumnHeader>Name</Table.ColumnHeader>
+            <Table.ColumnHeader>Department</Table.ColumnHeader>
+            <Table.ColumnHeader textAlign="end">Salary</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {data.map((item) => (
+            <Table.Row key={item.id}>
+              <Table.Cell>{item.name}</Table.Cell>
+              <Table.Cell>{item.department}</Table.Cell>
+              <Table.Cell textAlign="end">{item.salary}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+`;
+
 export const Default: Story = {
   render: (args) => (
     <Table {...args}>
-      <TableHeader />
-      <TableBody data={data} />
+      {TableHeader()}
+      {TableBody(data)}
     </Table>
   ),
+  parameters: {
+    docs: {
+      source: {
+        code: `
+import { Table } from "cocorig-ui";
+
+const Demo = () => {
+  return (
+  ${defaultCode}
+  );
+};
+`,
+      },
+    },
+  },
 };
 
+/**
+ * 테이블의 헤더를 고정시킬 수 있습니다.
+ *
+ * `Table`을 `Table.Container`로 감싸면, `Table.Container`에 적용된 `overflow: auto` 속성 때문에 `Table`의 기본 `border` 스타일이 적용되지 않습니다.
+ * 이로 인해 기존 `box-shadow`와 `border-radius` 스타일이 자동으로 제거됩니다.
+ *
+ * 따라서 테두리 스타일을 유지하려면, `Table.Container`에 `borderRadius`와 `borderWidth` 값을 설정하여 조정할 수 있습니다.
+ */
 export const PinnedHeader: Story = {
-  render: () => (
+  render: (args) => (
     <Table.Container height="150px" borderWidth={'1px'}>
       <Table>
-        <TableHeader />
-        <TableBody data={data} />
+        {TableHeader()}
+        {TableBody(data)}
       </Table>
     </Table.Container>
   ),
+
+  parameters: {
+    docs: {
+      source: {
+        code: `
+import { Table } from "cocorig-ui";
+
+const Demo = () => {
+  return (
+    <Table.Container height={"150px"} borderWidth={"1px"}>${defaultCode}    </Table.Container>
+  );
+};
+`,
+      },
+    },
+  },
 };
